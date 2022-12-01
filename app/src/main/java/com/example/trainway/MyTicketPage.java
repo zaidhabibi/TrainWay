54939874681ab14b7a5e05d511fee9dbcc99e81e
Zane Sanchez <zane.sanchez@protonmail.com>
2022-11-30 21:06:17 -0600

    Redoing Tickets

--- app/src/main/java/com/example/trainway/MyTicketPage.java
+++ app/src/main/java/com/example/trainway/MyTicketPage.java
@@ -29,16 +29,44 @@ import com.google.firebase.database.ValueEventListener;
 public class MyTicketPage extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
+        Button buy;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myticket);
+        // buy button
+        buy = findViewById(R.id.ticketBuy);
 
+        // destination spinner
         Spinner spinnerDestination = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_String_Destination, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinnerDestination.setAdapter(adapter);
+        // time spinner
         Spinner spinnerTime = findViewById(R.id.spinner2);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.spinner_Times, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinnerTime.setAdapter(adapter2);
+
+        //buy ticket
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createTicket(view);
+            }
+        });
     }
+        int x=1;
+        public void createTicket(View view)
+        {
+            // Write Ticket to Database
+
+            Spinner route =(Spinner)findViewById(R.id.spinner);
+            Spinner time =(Spinner)findViewById(R.id.spinner2);
+
+            // Copies user selection into ticket creator
+            TicketMain.Ticket test = new TicketMain.Ticket();
+            //test.newTicket("test",10,"Dallas","11/23","10:30",50);
+            test.newTicket(String.valueOf(x),10,route.getSelectedItem().toString(),"11/30",time.getSelectedItem().toString(),1);
+            x++;
+        }
+
 }
